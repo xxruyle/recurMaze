26947f44f7dfa1a8429df65b7aaa08ef45b7ce3a
xxruyle <xrcnerd@gmail.com>
2023-03-06 08:20:35 -0600

    starting genmaze

---
+++ recurMaze/genmaze.py
@@ -0,0 +1,10 @@
+import random 
+
+class Genmaze: 
+    '''Generates a maze''' 
+    def __init__(self): 
+        pass 
+
+    def generate(self, rows, cols) :
+        '''Generates a n rows x n cols maze'''
+        
